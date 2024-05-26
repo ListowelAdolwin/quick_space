61be4f01f90a06f38b417de27bb2a6be742158f9
ListowelAdolwin <99392098+ListowelAdolwin@users.noreply.github.com>
2024-05-26 20:10:46 +0000

    Delete client directory

--- client/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-function ErrorMessage(props) {
-	return (
-		<p className="mb-3 p-2 bg-red-800 text-white text-sm opacity-75 rounded-lg">
-			{props.errorMessage}
-		</p>
-	);
-}
-
-export default ErrorMessage;
